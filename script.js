88aefcc0c2fc8547002573d4396149112590c6e4
Ritik Sharma <ritiksharma10508@gmail.com>
2025-11-18 01:13:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
